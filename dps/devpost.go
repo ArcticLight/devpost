84ccaa8865006534f1777a0798dcbfba156f2b46
Max Clive <mattrulz127@gmail.com>
2015-05-06 04:01:07 -0400

    Add a few content-type guesses based on the last 3 characters of the filename served. Also, add the default of trying to serve index.html when the path would have been 404'd

--- dps/devpost.go
+++ dps/devpost.go
@@ -25,6 +25,25 @@ func init() {
     closereq = make(chan(bool), 1)
 }
 
+func guessContent(w http.ResponseWriter, path string) {
+    if(len(path) <= 4) {
+        return
+    }
+    
+    last3 := path[len(path)-3:]
+    
+    switch {
+        case last3 == "css":
+            w.Header().Set("Content-Type", "text/css")
+        case last3 == ".js":
+            w.Header().Set("Content-Type", "text/javascript")
+        case last3 == "tml":
+            w.Header().Set("Content-Type", "text/html")
+        case last3 == "svg":
+            w.Header().Set("Content-Type", "image/svg+xml")
+    }
+}
+
 func devpostHandler(w http.ResponseWriter, r *http.Request) {
     if(firstContact) {
         firstContact = false
@@ -42,15 +61,29 @@ func devpostHandler(w http.ResponseWriter, r *http.Request) {
                     }
             }
         } else {
-            path := r.URL.Path[1:]
-            if(os.PathSeparator != '/') {
+            path := "./" + r.URL.Path[1:]
+            if os.PathSeparator != '/' {
                 path = strings.Replace(path, "/", string(os.PathSeparator), -1)
             }
             contents, err := ioutil.ReadFile(r.URL.Path[1:])
-            if(err != nil) {
-                w.WriteHeader(404)
-                fmt.Fprintf(w, rez.FileNotFound(path, err))
+            if err != nil {
+                //fmt.Println(path[len(path)-2:])
+                //fmt.Println(path[:len(path)-1])
+                if len(path) >= 2 && path[len(path)-1:] == string(os.PathSeparator) {
+                    path = path[:len(path)-1]
+                } else if len(path) < 2 {
+                    path = ""
+                }
+                contents, err = ioutil.ReadFile(path + string(os.PathSeparator) + "index.html")
+                if err != nil {
+                    w.WriteHeader(404)
+                    fmt.Fprintf(w, rez.FileNotFound(path, err))
+                } else {
+                    w.Header().Set("Content-Type", "text/html")
+                    w.Write(contents)
+                }
             } else {
+                guessContent(w, path)
                 w.Write(contents)
             }
         }
